ea1d1001d032f567ba65027946e1692d1a82e34f
Zofia Tunova <zofka.tunova@gmail.com>
2023-01-15 19:46:07 +0100

    add shape structure

---
+++ shapes.py
@@ -0,0 +1,9 @@
+# contour
+# hierarchy
+# type rectangle, ellipse, triangle, diamond
+
+class Shape:
+    def __init__(self, contour, hierarchy, shape_name):
+        self.contour = contour
+        self.hierarchy = hierarchy
+        self.shape_name = shape_name
