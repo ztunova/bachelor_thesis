5b3ddc8498e71f66490529d348122521191ac495
Zofia Tunova <zofka.tunova@gmail.com>
2023-01-16 21:11:59 +0100

    add bounding rectangle as shape attribute

--- shapes.py
+++ shapes.py
@@ -1,9 +1,11 @@
 # contour
 # hierarchy
 # type rectangle, ellipse, triangle, diamond
+# bounding rectangle
 
 class Shape:
-    def __init__(self, contour, hierarchy, shape_name):
+    def __init__(self, contour, hierarchy, shape_name, bounding_rectangle):
         self.contour = contour
         self.hierarchy = hierarchy
         self.shape_name = shape_name
+        self.bounding_rectangle = bounding_rectangle
