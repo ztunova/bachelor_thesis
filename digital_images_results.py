f119d221e4950a571edd41a71e28cc091b014fe5
Zofia Tunova <zofka.tunova@gmail.com>
2023-01-17 20:27:59 +0100

    show detected lines after removal of shapes

--- digital_images_results.py
+++ digital_images_results.py
@@ -7,6 +7,7 @@ import cv2
 def show_results_html():
     original_images_dir = "C:/Users/zofka/OneDrive/Dokumenty/FEI_STU/bakalarka/dbs2022_riadna_uloha1_digital_resized"
     contours_dir = "C:/Users/zofka/OneDrive/Dokumenty/FEI_STU/bakalarka/dbs_ru1_digital_contours"
+    removed_shapes_dir = "C:/Users/zofka/OneDrive/Dokumenty/FEI_STU/bakalarka/dbs_ru1_removed_shapes"
 
     original_images_names = os.listdir(original_images_dir)
 
@@ -35,6 +36,7 @@ def show_results_html():
     for i in range(len(original_images_names)):
         orig_img_path = original_images_dir + '/' + original_images_names[i]
         contour_img_path = contours_dir + '/' + original_images_names[i]
+        removed_shapes_img_path = removed_shapes_dir + '/' + original_images_names[i]
 
         img = cv2.imread(orig_img_path)
         height, width = img.shape[:2]
@@ -43,6 +45,7 @@ def show_results_html():
         table_rov = "<tr>" \
                     "<td><img src= \"" + orig_img_path + "\" alt=\"Sth went wrong\" width=\"500\" height=\"400\"></td>" \
                     "<td><img src=\"" + contour_img_path + "\" alt=\"Sth went wrong\" width=\"500\" height=\"400\"></td>" \
+                    "<td><img src=\"" + removed_shapes_img_path + "\" alt=\"Sth went wrong\" width=\"500\" height=\"400\"></td>" \
                     "</tr>\n"
         html_table = html_table + table_rov_info + "\n" + table_rov
 
