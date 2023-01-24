bd7c337f4e82a73a77e480e2f48bffbe70bce1f1
Zofia Tunova <zofka.tunova@gmail.com>
2023-01-24 20:12:33 +0100

    add line class

--- shapes.py
+++ shapes.py
@@ -1,4 +1,5 @@
 # contour
+# enlarged contour - for line detection (convex hull of contour)
 # hierarchy
 # type rectangle, ellipse, triangle, diamond
 # bounding rectangle
@@ -6,6 +7,14 @@
 class Shape:
     def __init__(self, contour, hierarchy, shape_name, bounding_rectangle):
         self.contour = contour
+        self.enlarged_contour = None
         self.hierarchy = hierarchy
         self.shape_name = shape_name
         self.bounding_rectangle = bounding_rectangle
+
+
+class Line:
+    def __init__(self, contour, edge_points, color):
+        self.contour = contour
+        self.edge_points = edge_points
+        self.color = color
